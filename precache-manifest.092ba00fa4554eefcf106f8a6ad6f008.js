e5cb5f07550f6c8481d74fa2e8828d2b0df25b36
Pius <gyamenahyawpius@gmail.com>
2020-04-21 03:14:32 +0000

    Updates

---
+++ precache-manifest.092ba00fa4554eefcf106f8a6ad6f008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96068782f606d46d29520b9edf3a8e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b263121277908eac7bd",
+    "url": "/monsters-rolodex/static/css/main.1c08da51.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b263121277908eac7bd",
+    "url": "/monsters-rolodex/static/js/main.2a0bd77a.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
